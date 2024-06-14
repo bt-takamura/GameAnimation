9bc32fc6e8cca36e8fff2d88e5b3d2dc430f287c
BEE-TRIBE\takamura <takamura@bee-tribe.jp>
2024-06-14 17:21:57 +0900

    Sourceファイル追加

---
+++ Source/GameAnimationSample/GAGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GAGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEANIMATIONSAMPLE_API AGAGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
