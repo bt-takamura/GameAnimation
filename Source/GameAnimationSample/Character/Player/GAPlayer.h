b8aac167daf4a5f520dc5bae9ade50b490ccaf05
Satoshi_Nishimura <s104nishimura@gmail.com>
2024-06-17 15:41:41 +0900

    モーションマッチング用の検証マップとBPを追加

---
+++ Source/GameAnimationSample/Character/Player/GAPlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/Base/SNCharacterBase.h"
+#include "GAPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEANIMATIONSAMPLE_API AGAPlayer : public ASNCharacterBase
+{
+	GENERATED_BODY()
+	
+};
