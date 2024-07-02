fa4b3fc6d1a4ad4d8c3a095978539890333fcf06
BEE-TRIBE\takamura <takamura@bee-tribe.jp>
2024-07-02 17:55:34 +0900

    ABP、Database(Dense)、Chooserをサンプルと同じものを作成、動作問題なし

---
+++ Source/GameAnimationSample/Character/Player/GAClimbCharacter.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameAnimationSample/Character/Player/GAClimbCharacter.h"
+
+// Sets default values
+AGAClimbCharacter::AGAClimbCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AGAClimbCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGAClimbCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AGAClimbCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
